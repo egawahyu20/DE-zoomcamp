afb73ce537bfb5fdda261a839a1d356c5801e7d9
egawahyu20 <egawahyu20@gmail.com>
2024-03-05 15:33:26 +0000

    workflow-orchestration

--- 01-docker-terraform/1_terraform_gcp/terraform/terraform_with_variables/variables.tf
+++ 01-docker-terraform/1_terraform_gcp/terraform/terraform_with_variables/variables.tf
@@ -1,10 +1,9 @@
 variable "credentials" {
   description = "My Credentials"
-  default     = "../keys/google_credentials.json"
+  default     = "../../keys/google_credentials.json" 
   #ex: if you have a directory where this file is called keys with your service account json file
   #saved there as my-creds.json you could use default = "./keys/my-creds.json"
 }
-
 
 variable "project" {
   description = "Project"
